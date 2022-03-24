5998b20de39f31edefcc3bf07cb5d7d180734d11
VaqueroFontenla <loret.vaquero@gmail.com>
2022-03-24 18:27:07 +0100

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Header } from "./header"
-export { default as Layout } from "./layout"
-export { default as SEO } from "./seo"
-export { default as Jumbo } from "./jumbo"
-export { default as Suggestions } from "./suggestions"
-export { default as SuggestionDetail } from "./suggestionDetail"
-export { default as ScrollToTop } from "./scroll-to-top"
-export { default as ExpandIcon } from "./expand-icon"
--- src/templates/suggestion.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { SuggestionDetail } from "../components"
-
-export default function Suggestion({ pageContext }) {
-  return <SuggestionDetail {...pageContext} />
-}
